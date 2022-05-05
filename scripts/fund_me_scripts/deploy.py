728ac1902279e9cd2edb387cb39167a052bcffff
Ak <akalankapathirage22@gmail.com>
2022-05-05 13:16:05 +0530

    get_pricefeed() helper + brownie network configs for price feed addrs + publish source based on config

--- scripts/fund_me_scripts/deploy.py
+++ scripts/fund_me_scripts/deploy.py
@@ -1,10 +1,17 @@
-from brownie import FundMe
-from ...utilities.helpers import get_account
+from brownie import FundMe, config, network
+from ...utilities.helpers import get_account, get_pricefeed
+
 
 def deploy_fund_me():
     account = get_account()
-    fund_me = FundMe.deploy({"from": account})
+    price_feed_addresss = get_pricefeed()
+    fund_me = FundMe.deploy(
+        price_feed_addresss,
+        {"from": account},
+        publish_source=config["networks"][network.show_active()].get("verify"),
+    )
     print(f"Contract deployed to {fund_me.address}")
+
 
 def main():
     deploy_fund_me()
--- utilities/helpers.py
+++ utilities/helpers.py
@@ -1,7 +1,22 @@
-from brownie import network, config, accounts
+from brownie import network, config, accounts, MockV3Aggregator
+
+DECIMALS = 18
+STARTING_PRICE = 200000000000
+
 
 def get_account():
     if network.show_active() == "development":
         return accounts[0]
     else:
         return accounts.add(config["wallets"]["from_key"])
+
+
+def get_pricefeed():
+    if network.show_active() != "development":
+        return config["networks"]["rinkeby"]["eth_usd_price_feed"]
+    else:
+        print(f"The active network is {network.show_active()}")
+        print("Deploying Mocks...")
+        mockV3 = MockV3Aggregator.deploy(DECIMALS, STARTING_PRICE, {"from": get_account()})
+        print("Mocks Deployed!")
+        return mockV3.address
